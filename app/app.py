487c6fab0996f0b3a945d1ae4d9e1ea1859471db
Gianna Aprile <giannaaprile99@gmail.com>
2020-07-22 20:58:59 -0400

    set up for deployment to heroku

--- app/app.py
+++ app/app.py
@@ -3,91 +3,91 @@ from flask_cors import CORS
 from CreatePlaylist import relatedArtistPlaylistDriver, genrePlaylistDriver
 import sys
 
-application = Flask(__name__)
-CORS(application)
+app = Flask(__name__)
+CORS(app)
 
-@application.route("/")
+@app.route("/")
 def index():
     return jsonify({"hello": "world", "from": "index"})
 
-@application.route('/api/topRelatedArtists', methods=['GET'])
+@app.route('/api/topRelatedArtists', methods=['GET'])
 def topRelatedArtists():
     token = request.args.get('token')
     res = relatedArtistPlaylistDriver(token, 'topRelatedArtists')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/recentRelatedArtists', methods=['GET'])
+@app.route('/api/recentRelatedArtists', methods=['GET'])
 def recentRelatedArtists():
     token = request.args.get('token')
     res = relatedArtistPlaylistDriver(token, 'recentRelatedArtists')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/topRelatedTracks', methods=['GET'])
+@app.route('/api/topRelatedTracks', methods=['GET'])
 def topRelatedTracks():
     token = request.args.get('token')
     res = relatedArtistPlaylistDriver(token, 'topRelatedTracks')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/pop', methods=['GET'])
+@app.route('/api/pop', methods=['GET'])
 def pop():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'pop')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/hiphop', methods=['GET'])
+@app.route('/api/hiphop', methods=['GET'])
 def hiphop():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'hip hop')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/rap', methods=['GET'])
+@app.route('/api/rap', methods=['GET'])
 def rap():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'rap')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/r&b', methods=['GET'])
+@app.route('/api/r&b', methods=['GET'])
 def rNb():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'r&b')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/edm', methods=['GET'])
+@app.route('/api/edm', methods=['GET'])
 def edm():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'edm')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/house', methods=['GET'])
+@app.route('/api/house', methods=['GET'])
 def house():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'house')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/country', methods=['GET'])
+@app.route('/api/country', methods=['GET'])
 def country():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'country')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/alternative', methods=['GET'])
+@app.route('/api/alternative', methods=['GET'])
 def alternative():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'alternative')
     response = jsonify({'data': res})
     return response
 
-@application.route('/api/rock', methods=['GET'])
+@app.route('/api/rock', methods=['GET'])
 def rock():
     token = request.args.get('token')
     res = genrePlaylistDriver(token, 'rock')
@@ -95,5 +95,5 @@ def rock():
     return response
 
 if __name__ == '__main__':
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
